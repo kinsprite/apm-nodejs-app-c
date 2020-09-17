1d325863e2044b3f0487890816552a9d5f73034b
kinsprite <qxqs11@126.com>
2020-09-17 23:17:27 +0800

    healthz test

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const axios = require('axios').default;
-const apm = require('elastic-apm-node').start();
+const apm = require('elastic-apm-node').start({
+  ignoreUrls: ['/healthz'],
+});
 
 const app = require('express')();
+
+app.get('/healthz', function (req, res) {
+  res.send({
+    msg: 'OK',
+  });
+});
 
 app.get('/api/:msg', async function (req, res) {
   res.send({
