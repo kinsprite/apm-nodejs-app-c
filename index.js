bb6c70a46ebf5eb165c036df895be158e63deb89
kinsprite <qxqs11@126.com>
2020-09-21 00:07:26 +0800

    async error handling on Express 4.x

--- index.js
+++ index.js
@@ -5,6 +5,28 @@ const apm = require('elastic-apm-node').start({
 
 const app = require('express')();
 
+function logErrors (err, req, res, next) {
+  console.error(err.stack);
+  next(err);
+}
+
+function clientErrorHandler (err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send({ error: 'Something failed!' });
+  } else {
+    next(err);
+  }
+}
+
+function errorHandler (err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+}
+
+app.use(logErrors);
+app.use(clientErrorHandler);
+app.use(errorHandler);
+
 app.get('/healthz', function (req, res) {
   res.send({
     msg: 'OK',
